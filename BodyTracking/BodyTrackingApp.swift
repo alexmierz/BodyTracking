c0bc943475aceae9dd425827b7adbcb63e6f2a0d
reassign <snowboardz125@gmail.com>
2024-01-17 15:49:27 -0500

    Initial Commit

---
+++ BodyTracking/BodyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTrackingApp.swift
+//  BodyTracking
+//
+//  Created by Alex Mierzejewski on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BodyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
